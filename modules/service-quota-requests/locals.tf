d7a5c03d4013f5d34f2b6fdd562b093cd77289f3
Byungjin Park <posquit0.bj@gmail.com>
2021-04-15 15:31:25 +0900

    Add service-quota codes

--- modules/service-quota-requests/locals.tf
+++ modules/service-quota-requests/locals.tf
@@ -2,8 +2,12 @@ locals {
   # PRs to add more of these mappings are very welcome. For more information
   # on how to find the Service Code and Quota Code, see the README.md!
   quota_codes = {
-    "vpc/rules-per-nacl"          = "L-2AEEBF1A"
-    "vpc/security-groups-per-eni" = "L-2AFB9258"
-    "vpc/nat-gateways-per-az"     = "L-FE5A380F"
+    "vpc/rules-per-nacl"               = "L-2AEEBF1A"
+    "vpc/security-groups-per-eni"      = "L-2AFB9258"
+    "vpc/nat-gateways-per-az"          = "L-FE5A380F"
+    "vpc/subnets-per-vpc"              = "L-407747CB"
+    "vpc/internet-gateways-per-region" = "L-A4707A72"
+    "vpc/vpcs-per-region"              = "L-F678F1CE"
+    "ec2/eips-per-region"              = "L-0263D0A3"
   }
 }
