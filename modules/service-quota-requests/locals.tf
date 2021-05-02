5f562cef3314c8b9531a30efcf22dfb6c34512b1
Byungjin Park <posquit0.bj@gmail.com>
2021-05-03 01:45:01 +0900

    Add resource-groups quotas

--- modules/service-quota-requests/locals.tf
+++ modules/service-quota-requests/locals.tf
@@ -2,12 +2,16 @@ locals {
   # PRs to add more of these mappings are very welcome. For more information
   # on how to find the Service Code and Quota Code, see the README.md!
   quota_codes = {
+    # VPC
     "vpc/rules-per-nacl"               = "L-2AEEBF1A"
     "vpc/security-groups-per-eni"      = "L-2AFB9258"
     "vpc/nat-gateways-per-az"          = "L-FE5A380F"
     "vpc/subnets-per-vpc"              = "L-407747CB"
     "vpc/internet-gateways-per-region" = "L-A4707A72"
     "vpc/vpcs-per-region"              = "L-F678F1CE"
-    "ec2/eips-per-region"              = "L-0263D0A3"
+    # EC2
+    "ec2/eips-per-region" = "L-0263D0A3"
+    # Resource Groups
+    "resource-groups/resource-groups-per-account" = "L-2BAA18A0"
   }
 }
