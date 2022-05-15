8e5021129c1fb9b6839c8ebf0a21eaa1098fb2ad
Byungjin Park <posquit0.bj@gmail.com>
2022-05-16 00:20:11 +0900

    Refactor iam-user module

--- modules/iam-user/variables.tf
+++ modules/iam-user/variables.tf
@@ -1,118 +1,95 @@
 variable "name" {
-  description = "Desired name for the IAM user."
+  description = "(Required) Desired name for the IAM user."
   type        = string
 }
 
 variable "path" {
-  description = "Desired path for the IAM user."
+  description = "(Optional) Desired path for the IAM user."
   type        = string
   default     = "/"
 }
 
 variable "force_destroy" {
-  description = "When destroying this user, destroy even if it has non-Terraform-managed IAM access keys, login profile or MFA devices. Without force_destroy a user with non-Terraform-managed access keys and login profile will fail to be destroyed."
+  description = "(Optional) When destroying this user, destroy even if it has non-Terraform-managed IAM access keys, login profile or MFA devices. Without force_destroy a user with non-Terraform-managed access keys and login profile will fail to be destroyed."
   type        = bool
   default     = false
 }
 
 variable "permissions_boundary" {
-  description = "The ARN of the policy that is used to set the permissions boundary for the user."
+  description = "(Optional) The ARN of the policy that is used to set the permissions boundary for the user."
   type        = string
-  default     = ""
+  default     = null
 }
 
 variable "groups" {
-  description = "A list of IAM Groups to add the user to."
+  description = "(Optional) A list of IAM Groups to add the user to."
   type        = list(string)
   default     = []
 }
 
-variable "create_login_profile" {
-  description = "Whether to create IAM user login profile."
-  type        = bool
-  default     = true
-}
-
 variable "pgp_key" {
-  description = "Either a base-64 encoded PGP public key, or a keybase username in the form keybase:username. Used to encrypt password and access key."
+  description = "(Optional) Either a base-64 encoded PGP public key, or a keybase username in the form keybase:username. Used to encrypt password and access key."
   type        = string
   default     = ""
 }
 
-variable "password_length" {
-  description = "The length of the generated password"
-  type        = number
-  default     = 20
+variable "console_access" {
+  description = <<EOF
+  (Optional) The configuration of the AWS console access and password for the user. `console_access` block as defined below.
+    (Optional) `enabled` - Whether to activate the AWS console access and password.
+    (Optional) `password_length` - The length of the generated password. Only applies on resource creation. Default value is `20`.
+    (Optional) `password_reset_required` -  Whether the user should be forced to reset the generated password on first login. Defaults to `true`.
+  EOF
+  type        = any
+  default     = {}
 }
 
-variable "password_reset_required" {
-  description = "Whether the user should be forced to reset the generated password on first login."
-  type        = bool
-  default     = true
+variable "access_keys" {
+  description = <<EOF
+  (Optional) A list of Access Keys to associate with the IAM user. This is a set of credentials that allow API requests to be made as an IAM user. Each value of `access_keys` block as defined below.
+    (Required) `enabled` - Whether to activate the Access Key.
+  EOF
+  type        = list(map(bool))
+  default     = []
 }
 
-variable "create_access_key" {
-  description = "Whether to create IAM access key."
-  type        = bool
-  default     = false
-}
-
-variable "access_key_enabled" {
-  description = "Whether to activate IAM access key."
-  type        = bool
-  default     = true
-}
-
-variable "upload_ssh_key" {
-  description = "Whether to upload a public ssh key to the IAM user."
-  type        = bool
-  default     = false
-}
-
-variable "ssh_public_key_encoding" {
-  description = "Specifies the public key encoding format to use in the response. To retrieve the public key in ssh-rsa format, use SSH. To retrieve the public key in PEM format, use PEM."
-  type        = string
-  default     = "SSH"
-}
-
-variable "ssh_public_key" {
-  description = "The SSH public key. The public key must be encoded in ssh-rsa format or PEM format."
-  type        = string
-  default     = ""
-}
-
-variable "ssh_public_key_enabled" {
-  description = "Whether to activate the SSH public key."
-  type        = bool
-  default     = true
+variable "ssh_keys" {
+  description = <<EOF
+  (Optional) A list of SSH public keys to associate with the IAM user. Each value of `ssh_keys` block as defined below.
+    (Required) `public_key` - The SSH public key. The public key must be encoded in ssh-rsa format or PEM format.
+    (Optional) `encoding` - Specify the public key encoding format. Valid values are `SSH` and `PEM`. To retrieve the public key in ssh-rsa format, use `SSH`. To retrieve the public key in PEM format, use `PEM`.
+    (Optional) `enabled` - Whether to activate the SSH public key.
+  EOF
+  type        = any
+  default     = []
 }
 
 variable "assumable_roles" {
-  description = "List of IAM roles ARNs which can be assumed by the user."
+  description = "(Optional) List of IAM roles ARNs which can be assumed by the user."
   type        = list(string)
   default     = []
 }
 
 variable "policies" {
-  description = "List of IAM policies ARNs to attach to IAM user."
+  description = "(Optional) List of IAM policies ARNs to attach to IAM user."
   type        = list(string)
   default     = []
 }
 
 variable "inline_policies" {
-  description = "Map of inline IAM policies to attach to IAM user. (`name` => `policy`)."
+  description = "(Optional) Map of inline IAM policies to attach to IAM user. (`name` => `policy`)."
   type        = map(string)
   default     = {}
 }
 
 variable "tags" {
-  description = "A map of tags to add to all resources."
+  description = "(Optional) A map of tags to add to all resources."
   type        = map(string)
   default     = {}
 }
 
 variable "module_tags_enabled" {
-  description = "Whether to create AWS Resource Tags for the module informations."
+  description = "(Optional) Whether to create AWS Resource Tags for the module informations."
   type        = bool
   default     = true
 }
@@ -123,19 +100,19 @@ variable "module_tags_enabled" {
 ###################################################
 
 variable "resource_group_enabled" {
-  description = "Whether to create Resource Group to find and group AWS resources which are created by this module."
+  description = "(Optional) Whether to create Resource Group to find and group AWS resources which are created by this module."
   type        = bool
   default     = true
 }
 
 variable "resource_group_name" {
-  description = "The name of Resource Group. A Resource Group name can have a maximum of 127 characters, including letters, numbers, hyphens, dots, and underscores. The name cannot start with `AWS` or `aws`."
+  description = "(Optional) The name of Resource Group. A Resource Group name can have a maximum of 127 characters, including letters, numbers, hyphens, dots, and underscores. The name cannot start with `AWS` or `aws`."
   type        = string
   default     = ""
 }
 
 variable "resource_group_description" {
-  description = "The description of Resource Group."
+  description = "(Optional) The description of Resource Group."
   type        = string
   default     = "Managed by Terraform."
 }
