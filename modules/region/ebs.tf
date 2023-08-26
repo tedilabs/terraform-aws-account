c43e48b94b9cbb3b03c3a1bec11594ac463e7df1
Byungjin Park <posquit0.bj@gmail.com>
2023-08-27 01:26:09 +0900

    Unify variables for ebs default encryption in reigon module

--- modules/region/ebs.tf
+++ modules/region/ebs.tf
@@ -3,11 +3,11 @@
 ###################################################
 
 resource "aws_ebs_encryption_by_default" "this" {
-  enabled = var.ebs_default_encryption_enabled
+  enabled = var.ebs_default_encryption.enabled
 }
 
 resource "aws_ebs_default_kms_key" "this" {
-  count = var.ebs_default_encryption_kms_key != null ? 1 : 0
+  count = var.ebs_default_encryption.kms_key != null ? 1 : 0
 
-  key_arn = var.ebs_default_encryption_kms_key
+  key_arn = var.ebs_default_encryption.kms_key
 }
