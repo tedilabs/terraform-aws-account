e9d365947591fe09cc1e826d5951508dd9da9bd6
Byungjin Park <posquit0.bj@gmail.com>
2024-04-17 17:40:21 +0900

    Migrate org and sso related modules to terraform-aws-organization

--- modules/sso-access-control-attributes/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "attributes" {
-  description = "(Optional) A map of attributes for access control are used in permission policies that determine who in an identity source can access your AWS resources."
-  type        = map(string)
-  default     = {}
-  nullable    = false
-}
